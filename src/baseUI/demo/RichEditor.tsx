c0ddd5c189bdc2913127d14ac667678bb32a6723
zhanggongying <edsolater@qq.com>
2020-10-20 16:27:24 +0800

    已实现纯文本本加粗（标签嵌套不算）、文字插入的逻辑

--- src/baseUI/demo/RichEditor.tsx
+++ src/baseUI/demo/RichEditor.tsx
@@ -1,8 +1,8 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import betweenNumberRange from 'functions/betweenNumberRange'
-import { assertTextNode } from 'functions/typeGards'
-import { FC, ReactElement, useLayoutEffect, useRef, useState } from 'react'
+import { isTextNode } from 'functions/typeGards'
+import { FC, ReactElement, useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 type IRangeInfo = {
   // 划线区的开头，相对与整段文字内容的位置
@@ -10,78 +10,123 @@ type IRangeInfo = {
   // 划线区的结尾，相对与整段文字内容的位置
   end: number
 }
+
+/**
+ * 获取某个节点内部的所有文字节点
+ * @param targetRootNode 目标节点
+ */
+function getAllTextNodesFromNode(targetRootNode: Node) {
+  let resultStack = [targetRootNode]
+  while (!resultStack.every(isTextNode)) {
+    // TODO: 计算性能优化。下述做了很多无用判断。理论上O(n)应该是可以做到的，
+    resultStack = resultStack.flatMap(node =>
+      isTextNode(node) ? node : Array.from(node.childNodes)
+    )
+  }
+  return resultStack
+}
+
 /**
  * 将记录下的range的应用到界面上（抄的，暂时还没看懂）
  */
 function applyLastRange(editor: HTMLElement, { start, end }: IRangeInfo) {
   var charIndex = 0
   const newRange = document.createRange()
-  newRange.setStart(editor, 0)
-  newRange.collapse(true)
-  const nodeStack: Node[] = [editor]
-  let foundStart = false // 记录是否已找到了选区的开始处
+  const textNodeStack = getAllTextNodesFromNode(editor)
+  let foundStart = false // 记录是否已找到了选区的开始位置
 
-  while (nodeStack.length) {
-    const node = nodeStack.pop()!
-    if (assertTextNode(node)) {
-      if (!foundStart && betweenNumberRange(start, [charIndex, charIndex + node.length])) {
-        newRange.setStart(node, start - charIndex)
-        foundStart = true
-      }
-      if (foundStart && betweenNumberRange(end, [charIndex, charIndex + node.length])) {
-        newRange.setEnd(node, end - charIndex)
-        // 至此，选取的开始与结尾都记录完毕，不需要继续遍历了
-        break
-      }
-      charIndex += node.length
-    } else {
-      nodeStack.push(...Array.from(node.childNodes))
+  newRange.setStart(editor, 0) //设定好默认的选区开始位置
+  console.log('start,end: ', start, end)
+  for (let i = 0; i < textNodeStack.length; i++) {
+    const textNode = textNodeStack[i]
+    if (!foundStart && betweenNumberRange(start, [charIndex, charIndex + textNode.length])) {
+      newRange.setStart(textNode, start - charIndex)
+      foundStart = true
     }
+    if (foundStart && betweenNumberRange(end, [charIndex, charIndex + textNode.length])) {
+      newRange.setEnd(textNode, end - charIndex)
+      break // 至此，选取的开始与结尾都记录完毕，不需要继续遍历了
+    }
+    charIndex += textNode.length
   }
+  console.log('newRange: ', newRange)
+
   const selection = getSelection()
+  // 先记录因此导致的selectionChange事件非用户发起的
   selection?.removeAllRanges()
   selection?.addRange(newRange)
 }
+
 /**
- * 想做个富文本编辑器
+ * 基于选区与文字，加粗
+ */
+function blodText(innerHTML: string, { start, end }: IRangeInfo): string {
+  const newInnerHTML = `${innerHTML.slice(0, start)}<b>${innerHTML.slice(
+    start,
+    end
+  )}</b>${innerHTML.slice(end)}`
+  return newInnerHTML
+}
+
+/**
+ * 记录下一些range的信息
+ */
+function getRangeInfo(editor: HTMLDivElement, range: Range) {
+  const preSelectionRange = range.cloneRange()
+  // 创建选择原选区文字左边的选区（目的是方便计算相对于所有文字的start/end）
+  preSelectionRange.selectNodeContents(editor)
+  preSelectionRange.setEnd(range.startContainer, range.startOffset)
+  const start = preSelectionRange.toString().length
+  return {
+    start,
+    end: start + range.toString().length
+  }
+}
+
+/**
+ * 富文本编辑器
  */
 const RichEditor: FC<{ clildren?: ReactElement }> = () => {
+  //#region ------------------- 组件的内部状态 -------------------
   // 编辑器框的应用
-  const containerRef = useRef<HTMLDivElement>(null)
+  const editorRef = useRef<HTMLDivElement>(null)
   // 保存上一个选取范围的信息
   const lastRangeInfo = useRef<IRangeInfo>()
   // 编辑器内部的文本信息
   const [innerHTML, setInnerHTML] = useState('这是一段没有意义的文字')
-  useLayoutEffect(() => {
-    if (containerRef.current && lastRangeInfo.current)
-      applyLastRange(containerRef.current, lastRangeInfo.current)
-  }, [innerHTML])
+  //#endregion
 
-  const syncInnerHTML = () => {
-    /**
-     * 记录下一些range的信息
-     */
-    const range = getSelection()?.getRangeAt(0)
-    if (range && containerRef.current) {
-      // 创建选择原选区文字左边的选区（目的是方便计算相对于所有文字的start/end）
-      const preSelectionRange = range.cloneRange()
-      preSelectionRange.selectNodeContents(containerRef.current)
-      preSelectionRange.setEnd(range.startContainer, range.startOffset)
-      const start = preSelectionRange.toString().length
-
-      lastRangeInfo.current = {
-        start,
-        end: start + range.toString().length
-      }
-    }
-    // 同步（记录） innerHTML
-    if (containerRef.current) {
-      setInnerHTML(containerRef.current.innerHTML)
-    }
+  //#region ------------------- 组件内部方法 -------------------
+  //同步innerHTML也会记录下选区的信息
+  const recordRange = (editor: HTMLDivElement | null, range: Range | undefined) => {
+    if (!editor || !range) return
+    const info = getRangeInfo(editor, range)
+    console.log('info: ', info)
+    lastRangeInfo.current = info
   }
+  const syncInnerHTML = () => {
+    recordRange(editorRef.current, getSelection()?.getRangeAt(0))
+    if (editorRef.current) setInnerHTML(editorRef.current.innerHTML)
+  }
+  //#endregion
+
+  //#region ------------------- effect/layoutEffect -------------------
+  // 只要有更新内容，就会触发对光标选区的重新计算
+  useLayoutEffect(() => {
+    if (editorRef.current && lastRangeInfo.current)
+      applyLastRange(editorRef.current, lastRangeInfo.current)
+  }, [innerHTML])
+  // 改变选区的 selectionChange 事件只能挂载在 document 上
+  // 无用功：有代码应用上一选区时，理论上无需再次记录选区
+  useEffect(() => {
+    document.addEventListener('selectionchange', () => {
+      recordRange(editorRef.current, getSelection()?.getRangeAt(0))
+    })
+  }, [])
+  //#endregion
   return (
     <div
-      ref={containerRef}
+      ref={editorRef}
       css={css({
         backgroundColor: '#eee',
         outline: 'none',
@@ -103,28 +148,13 @@ const RichEditor: FC<{ clildren?: ReactElement }> = () => {
           syncInnerHTML()
         }
       }}
-      // onKeyDown={e => {
-      //   if (e.ctrlKey && e.key.toLowerCase() === 'b') {
-      //     // 阻止弹出我的收藏夹文件夹
-      //     e.preventDefault()
-
-      //     const selection = getSelection()
-      //     // 选区的范围
-      //     const range = selection?.getRangeAt(0)
-      //     if (selection && containerRef.current) {
-      //       const start = selection.anchorOffset
-      //       const end = selection.focusOffset
-      //       const rangeText = selection.getRangeAt(0).toString()
-      //       const parentNode = selection.anchorNode?.parentElement
-      //       const parentTextNode = selection.anchorNode as Text
-
-      //       const b = document.createElement('b')
-      //       b.appendChild(document.createTextNode(rangeText))
-      //       parentNode?.insertBefore(b, parentTextNode.splitText(start))
-      //       parentNode?.removeChild(parentTextNode.splitText(end))
-      //     }
-      //   }
-      // }}
+      onKeyDown={e => {
+        if (e.ctrlKey && e.key.toLowerCase() === 'b') {
+          // 阻止弹出我的收藏夹文件夹
+          e.preventDefault()
+          if (lastRangeInfo.current) setInnerHTML(blodText(innerHTML, lastRangeInfo.current))
+        }
+      }}
       contentEditable
       dangerouslySetInnerHTML={{ __html: innerHTML }}
     ></div>
--- src/functions/typeGards.ts
+++ src/functions/typeGards.ts
@@ -2,6 +2,6 @@
  *
  * @param params 判断节点类型
  */
-export function assertTextNode(node: Node): node is Text {
+export function isTextNode(node: Node): node is Text {
   return node.nodeType === Node.TEXT_NODE
 }
