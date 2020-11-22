e7b7dd2b31d1cb351ac4b1be0b8ac64d9a99a983
zhanggongying <edsolater@qq.com>
2020-11-23 00:32:11 +0800

    试图提取add/removeEventListeners

--- src/functions/attachGestureScale.ts
+++ src/functions/attachGestureScale.ts
@@ -17,16 +17,18 @@ export default function attachGestureScale(
   // TODO: 感觉这种带有自动过期行为的Map，可以抽为一个数据结构，暂时简单点，用永不过期
   // const activePointers = new Map<TouchEvent['pointerId'], TouchEvent>()
   let initDistance = 0
-  //TODO：scale orgin
-  function touchStart(ev: TouchEvent) {
+  // TODO：scale orgin
+  function touchstart(ev: TouchEvent) {
+    // FIXME 这有问题：屏幕中只能出现2根手指，才能生效
     if (ev.touches.length === 2 && areSame(...toArray(ev.touches).map(extract('target')))) {
+      // TODO: 感觉需要增加灵活度，让extract也能接收一个对象数组
       const dx = calcDistance(...toArray(ev.touches, extract('clientX')))
       const dy = calcDistance(...toArray(ev.touches, extract('clientY')))
       initDistance = calcHypotenuse(dx, dy)
-      document?.addEventListener('touchmove', touchMove)
+      addEventListeners(document, { touchmove })
     }
   }
-  function touchMove(ev: TouchEvent) {
+  function touchmove(ev: TouchEvent) {
     if (ev.touches.length === 2) {
       ev.stopPropagation()
       const dx = calcDistance(...toArray(ev.touches, extract('clientX')))
@@ -35,10 +37,43 @@ export default function attachGestureScale(
       eventHandler(ev, { scale: newDistance / initDistance })
     }
   }
-  function touchEnd(ev: TouchEvent) {
-    document.removeEventListener('touchmove', touchMove)
+  function touchend() {
+    removeEventListeners(document, { touchmove })
   }
-  el?.addEventListener('touchstart', touchStart)
-  el?.addEventListener('touchend', touchEnd)
+  addEventListeners(el, { touchstart, touchend })
 }
 
+// TODO 可选地: 绑定 start/up 需要能自动 remove 掉 end/down
+function addEventListeners(
+  el: Document,
+  events: Partial<{ [eventName in keyof DocumentEventMap]: any }>
+)
+function addEventListeners(
+  el: HTMLElement | null,
+  events: Partial<{ [eventName in keyof HTMLElementEventMap]: any }>
+)
+function addEventListeners(
+  el: HTMLElement | null | Document,
+  events: Partial<{ [eventName in keyof HTMLElementEventMap]: any }>
+) {
+  Object.entries(events).forEach(([eventName, handler]) => {
+    el?.addEventListener(eventName, handler)
+  })
+}
+
+function removeEventListeners(
+  el: Document,
+  events: Partial<{ [eventName in keyof DocumentEventMap]: any }>
+)
+function removeEventListeners(
+  el: HTMLElement | null,
+  events: Partial<{ [eventName in keyof HTMLElementEventMap]: any }>
+)
+function removeEventListeners(
+  el: HTMLElement | null | Document,
+  events: Partial<{ [eventName in keyof HTMLElementEventMap]: any }>
+) {
+  Object.entries(events).forEach(([eventName, handler]) => {
+    el?.removeEventListener(eventName, handler)
+  })
+}
