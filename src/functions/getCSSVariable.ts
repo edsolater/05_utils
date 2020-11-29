6dd12633319116d11b22d1dffd34138bd8b22cfa
edsolater <edsolater@qq.com>
2020-11-29 17:45:37 +0800

    使用typescript4.1

--- src/functions/getCSSVariable.ts
+++ src/functions/getCSSVariable.ts
@@ -2,11 +2,11 @@
  * 获取CSS Variable
  */
 export default function getCSSVariable<T>(
-  el: HTMLElement,
+  el: HTMLElement | null,
   variableName: string,
   parser?: (value: string) => T
 ): T extends Object ? T : string {
-  const gettedValue = el.style.getPropertyValue(variableName)
+  const gettedValue = el?.style.getPropertyValue(variableName) ?? ''
   //@ts-ignore
   return parser ? parser(gettedValue) : gettedValue
 }
