2c9a283123a85a2cc1ff33a32d4bb89c899eab3f
edsolater <edsolater@qq.com>
2021-01-05 00:27:18 +0800

    用错了，不应该使用dragAndDrop进行拖动重排

--- src/ATestGrounds/TestDaggableList.tsx
+++ src/ATestGrounds/TestDaggableList.tsx
@@ -3,48 +3,42 @@
  ******************************/
 import notNullish from 'functions/judgers/notNullish'
 import React, { useEffect, useRef } from 'react'
-import Div from '../baseUI/Div'
+import Div, { DivProps } from '../baseUI/Div'
 
 const draggingElements = new Map<string, HTMLElement>()
 
+// SHUT 用错了，不应该使用dragAndDrop进行拖动重排
 const TestDaggableList = () => {
+  const draggableItemCSS: DivProps['css'] = {
+    padding: 16,
+    margin: 8,
+    background: 'lightgray'
+  }
+  const draggingItemIndex = useRef<number>()
   return (
     <Div css={{ display: 'grid', gap: 8 }}>
-      <Div
-        css={{ padding: 16, margin: 8, background: 'lightgray' }}
-        draggable
-        onDragEnter={preventDefault}
-      >
-        South Sudan
-      </Div>
-      <Div
-        css={{ padding: 16, margin: 8, background: 'lightgray' }}
-        draggable
-        onDragEnter={preventDefault}
-      >
-        Guernsey
-      </Div>
-      <Div
-        css={{ padding: 16, margin: 8, background: 'lightgray' }}
-        draggable
-        onDragEnter={preventDefault}
-      >
-        Benin
-      </Div>
-      <Div
-        css={{ padding: 16, margin: 8, background: 'lightgray' }}
-        draggable
-        onDragEnter={preventDefault}
-      >
-        Russia
-      </Div>
-      <Div
-        css={{ padding: 16, margin: 8, background: 'lightgray' }}
-        draggable
-        onDragEnter={preventDefault}
-      >
-        El Salvador
-      </Div>
+      {['AAAA', 'BBBB', 'CCC', 'DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD'].map((text, index) => (
+        <Div
+          css={[
+            draggableItemCSS,
+            draggingItemIndex.current === index && { position: 'absolute', left: 10000 }
+          ]}
+          key={index}
+          draggable
+          onMouseDown={preventDefault}
+          onDragEnter={preventDefault}
+          onDragStart={e => {
+            draggingItemIndex.current = index
+            const cloned = (e.target as HTMLDivElement).cloneNode(true) as HTMLDivElement
+            cloned.style.setProperty('position', 'absolute')
+            cloned.style.setProperty('left', '10000px')
+            document.body.appendChild(cloned)
+            e.dataTransfer.setDragImage(cloned, 0, 0)
+          }}
+        >
+          {text}
+        </Div>
+      ))}
     </Div>
   )
 }
