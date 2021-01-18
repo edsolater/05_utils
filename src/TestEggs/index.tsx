d0eb1f04aebad360781072c3a6d49bc362dbd8d1
zhanggongying <edsolater@qq.com>
2021-01-18 10:47:31 +0800

    mixin可用智能提示，但好像代码反而不易读了

--- src/TestEggs/index.tsx
+++ src/TestEggs/index.tsx
@@ -1,23 +1,22 @@
 import Div from 'baseUI/Div'
-import { CSStestGridContainer, CSStestGridItem } from 'css/mixin/grid'
-import { CSSgridItemTextLabel } from 'css/mixin/text'
 import React from 'react'
+import { applyMixins } from 'style/cssMixins'
 import CollapseWatcher from './CollapseWatcher'
 import GroupScroll from './groupScroll'
 import SortableList from './SortableList'
 
 const TestGrounds = () => (
-  <Div css={CSStestGridContainer}>
-    <Div css={CSStestGridItem}>
-      <Div css={CSSgridItemTextLabel()}>拖拽排序</Div>
+  <Div css={applyMixins({testGridContainer:[]})}>
+    <Div css={applyMixins({testGridItem:[]})}>
+      <Div css={applyMixins({gridItemTextLabel:[]})}>拖拽排序</Div>
       <SortableList />
     </Div>
-    <Div css={CSStestGridItem}>
-      <Div css={CSSgridItemTextLabel()}>多item碰撞检测</Div>
+    <Div css={applyMixins({testGridItem:[]})}>
+      <Div css={applyMixins({gridItemTextLabel:[]})}>多item碰撞检测</Div>
       <CollapseWatcher />
     </Div>
-    <Div css={CSStestGridItem}>
-      <Div css={CSSgridItemTextLabel()}>整片翻页</Div>
+    <Div css={applyMixins({testGridItem:[]})}>
+      <Div css={applyMixins({gridItemTextLabel:[]})}>整片翻页</Div>
       <GroupScroll
         items={[
           'Dollie',
@@ -73,7 +72,7 @@ const TestGrounds = () => (
         )}
       />
     </Div>
-    <Div css={CSStestGridItem}></Div>
+    <Div css={applyMixins({testGridItem:[]})}></Div>
   </Div>
 )
 export default TestGrounds
--- src/style/cssMixins.ts
+++ src/style/cssMixins.ts
@@ -1,2 +1,34 @@
+import { Interpolation } from '@emotion/core'
 
 // TODO: 放预定义的各种CSS组合
+export const allCSSMixins = {
+  gridItemTextLabel: (): Interpolation => ({
+    position: 'absolute',
+    left: '50%',
+    top: 0,
+    transform: 'translateX(-50%)',
+    fontSize: 34,
+    color: 'gray'
+  }),
+  testGridContainer: (): Interpolation => ({
+    display: 'grid',
+    gridTemplate: '1fr 1fr / 1fr 1fr',
+    gap: 8,
+    overflow: 'hidden',
+    background: 'lightgray',
+    height: '100vh'
+  }),
+  testGridItem: (): Interpolation => ({
+    background: 'white',
+    position: 'relative',
+    overflow: 'hidden'
+  })
+} 
+export type AllMixins = ['']
+export function applyMixins(
+  mixins: { [mixinName in keyof typeof allCSSMixins]?: Parameters<typeof allCSSMixins[mixinName]> }
+): Interpolation {
+  return Object.entries(mixins)
+    .map(([mixinName, mixinParams]) => allCSSMixins[mixinName]([...(mixinParams ?? [])]))
+    .reduce((acc, cssRules) => ({ ...acc, ...cssRules }), {})
+}
