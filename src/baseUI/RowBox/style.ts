7e5e578e6910ef6417419c5616d06acbcc3ad886
zhanggongying <edsolater@qq.com>
2021-03-26 21:31:51 +0800

    对RowBox，已经自称体系了

--- src/baseUI/RowBox/style.ts
+++ src/baseUI/RowBox/style.ts
@@ -1,22 +1,26 @@
 import { mix } from 'style/cssMixins'
 import { ICSS } from 'style/cssType'
-import { toPx } from 'style/cssUnits'
 
-export interface RowBoxCSSPart {
-  small?: ICSS
-  middle?: ICSS
-  large?: ICSS
-  noStratch?: ICSS
-}
+// 声明组件有哪些props是服务于样式的
 export interface RowBoxStyleProps {
   /**对组件的每一个part或虚拟part定义样式 */
-  cssPart?: RowBoxCSSPart
+  cssPart?: {
+    small?: ICSS
+    middle?: ICSS
+    large?: ICSS
+    noStratch?: ICSS
+  }
   /**
    * "横"盒子空隙的大小
    */
   gapSize?: 'small' | 'middle' | 'large'
   noStratch?: boolean
 }
+
+// 表明具体有哪些props是服务于样式的（JS代码声明，也便与提取相关属性）
+export const rowBoxStylePropNames: (keyof RowBoxStyleProps)[] = ['cssPart', 'gapSize', 'noStratch']
+
+// 样式的具体css-in-js实现
 // BaseUI的样式：只提供能在黑白视图中，瞬间明白这玩意儿是干啥用的基础界面UI：
 export const cssRowBoxBaseStyle = ({ gapSize = 'middle', cssPart, noStratch }: RowBoxStyleProps) =>
   mix(
---
+++ src/utils/object/omit.ts
@@ -0,0 +1,20 @@
+/**
+ * 几乎是Typescript的Omit的翻版
+ * @param targetObj 需要浅复制的对象
+ * @param dismissKeys 需要去除的property（不支持去除sumbol）
+ * @returns 已经去除指定key的对象（浅复制得来）
+ * @example
+ * const obj = { a: 1, b: true }
+ * const foo = omit(obj, ['a'])
+ * console.log(foo)
+ */
+export default function omit<T extends object, U extends keyof T>(
+  obj: T,
+  keys: U[]
+): { [P in Exclude<keyof T, U>]: T[P] } {
+  const newObj = { ...obj }
+  keys.forEach((key) => {
+    delete newObj[key]
+  })
+  return newObj
+}
