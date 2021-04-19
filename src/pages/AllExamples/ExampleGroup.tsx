045c963b82a07030735707004bc7f0db2df97a29
edsolater <edsolater@qq.com>
2021-04-19 21:12:17 +0800

    固定组件写法，用function可读性更高

--- src/pages/AllExamples/ExampleGroup.tsx
+++ src/pages/AllExamples/ExampleGroup.tsx
@@ -1,16 +1,19 @@
 import Caption from 'baseUI/Caption'
-import Div from 'baseUI/Div'
+import Div, { DivProps } from 'baseUI/Div'
 import RowBox from 'baseUI/RowBox'
-import React, { ReactNode } from 'react'
-
-const ExampleGroup = (props: { children?: ReactNode; caption?: string }) => (
-  <Div>
-    <Caption css={{ margin: '16px 0', fontSize: '1.2em', fontWeight: 'bold' }}>
-      {props.caption}
-    </Caption>
-    <Div css={{ display: 'grid', gap: 8 }}>
-      <RowBox noStratch>{props.children}</RowBox>
+import React from 'react'
+export interface ExampleGroupProps extends DivProps {
+  caption?: string
+}
+export default function ExampleGroup(props: ExampleGroupProps) {
+  return (
+    <Div>
+      <Caption css={{ margin: '16px 0', fontSize: '1.2em', fontWeight: 'bold' }}>
+        {props.caption}
+      </Caption>
+      <Div css={{ display: 'grid', gap: 8 }}>
+        <RowBox noStratch>{props.children}</RowBox>
+      </Div>
     </Div>
-  </Div>
-)
-export default ExampleGroup
+  )
+}
