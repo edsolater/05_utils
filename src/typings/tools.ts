80d9f4d549549d239b923340ddd027d4cdb12e42
edsolater <edsolater@qq.com>
2021-05-05 23:50:10 +0800

    新增XOR类型方程

--- src/typings/tools.ts
+++ src/typings/tools.ts
@@ -15,7 +15,7 @@ export type MayFunction<T> = T | (() => T)
  * @example
  * const e = MayEnum<'hello'|'world'> // 'hello' | 'world' | (string & {})
  */
-export type MayEnum<T> = T | (string & {})
+export type SuggestString<T> = T | (string & {})
 
 /**
  * type I = GetRequired<{ foo: number, bar?: string }> // expected to be { foo: number }
@@ -27,12 +27,31 @@ export type MayEnum<T> = T | (string & {})
  */
 // type GetOptional<T> = {[P in keyof T as T[P] extends Required<T>[P] ? never: P]: T[P]}
 
-// 获取对象的所有非方法的属性名
+/**
+ * 获取对象的所有非方法的属性名
+ * @example
+ * Properties<{a: number, b: true, c(): boolean}> // 'a' | 'b'
+ */
 export type Properties<O, T = keyof O> = T extends keyof O
   ? O[T] extends () => void
     ? never
     : T
   : never
 
-// 获取对象的所有方法名
+/**
+ * 获取对象的所有方法名
+ * @example
+ * Properties<{a: number, b: true, c(): boolean}> // 'c'
+ */
 export type Methods<O> = Exclude<keyof O, Properties<O>>
+
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: undefined }
+/**
+ * 两个类型互斥（只能二选一）
+ * @example
+ * type F = XOR<{ d: '2' }, { a: 1; b: true; c(): boolean }>
+ * const d: F = { a: 1, b: true, c: () => true } // OK
+ * const d: F = { d: '2' } // OK
+ * const d: F = { a: 1, b: true, c: () => true, d: '2' } // Error
+ */
+export type XOR<T, U> = T | U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U
