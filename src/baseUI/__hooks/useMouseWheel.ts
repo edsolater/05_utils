01cfb84957c06e1be0da3ac6f2f677bbac079925
edsolater <edsolater@qq.com>
2021-05-15 00:35:30 +0800

    更改了一些hook的写法，更灵活了

--- src/baseUI/__hooks/useMouseWheel.ts
+++ src/baseUI/__hooks/useMouseWheel.ts
@@ -1,22 +1,46 @@
-import { useEffect, useRef } from 'react'
+import { RefObject, useEffect, useRef, useState } from 'react'
 
+type CallbackInfo = {
+  scrollDistance: number
+}
+
+/**
+ * 监听元素上鼠标滚轮的滚动
+ * @param ref 需要检测的元素
+ * @param options
+ * @returns hooks的结果
+ */
 export default function useMouseWheel(
-  ref: React.RefObject<HTMLElement>,
-  cb?: (info: { eventObject: WheelEvent; distance: number }) => void
-) {
-  const eventObjectRef = useRef<WheelEvent>(null)
+  ref: RefObject<HTMLElement>,
+  options?: {
+    /**
+     * 如此hooks无需返回，则使用Ref保存状态，而不是会触发重渲染的State
+     * 此属性不可变动
+     */
+    noReturn?: boolean
+    cb?: (info: CallbackInfo | { eventObject: WheelEvent }) => void
+    //TODO: 间隔时间功能函数。 intervalTime: 300
+  }
+): CallbackInfo {
+  const [_, setForceScrollDistance] = options?.noReturn ? [] : useState(0) // 为了纯粹使用hooks时能触发重渲
   const scrollDistance = useRef(0)
 
   useEffect(() => {
-    const updateScroll = (e: WheelEvent) => {
-      const newDistance = e.pageY + scrollDistance.current
-      cb?.({ eventObject: e, distance: newDistance })
-      scrollDistance.current = newDistance
+    /**
+     * 从鼠标滚轮事件中更新
+     * @param ev 鼠标滚轮
+     */
+    const updateScroll = (ev: WheelEvent) => {
+      setForceScrollDistance?.((oldDistance) => oldDistance + ev.pageY) // 为了纯粹使用hooks时能触发重渲
+      scrollDistance.current += ev.pageY
+      options?.cb?.({ eventObject: ev, scrollDistance: scrollDistance.current })
     }
-    ref.current?.addEventListener('wheel', updateScroll)
-  })
 
-  return { eventObject: eventObjectRef.current }
+    ref.current?.addEventListener('wheel', updateScroll)
+    return () => ref.current?.removeEventListener('wheel', updateScroll)
+  }, [options])
+
+  return { scrollDistance: scrollDistance.current }
 }
 
 //IDEA： 还可以代替Div的.feature.ts
