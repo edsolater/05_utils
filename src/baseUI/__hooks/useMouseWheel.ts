a12bf42248f05db38fe0ce0c8bc9c943204bc890
edsolater <edsolater@qq.com>
2021-05-14 23:41:47 +0800

    更改了一些hook的写法，更规范了

--- src/baseUI/__hooks/useMouseWheel.ts
+++ src/baseUI/__hooks/useMouseWheel.ts
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from 'react'
 
 export default function useMouseWheel(
-  ref: React.MutableRefObject<HTMLElement | null>,
+  ref: React.RefObject<HTMLElement>,
   cb?: (info: { eventObject: WheelEvent; distance: number }) => void
 ) {
   const eventObjectRef = useRef<WheelEvent>(null)
   const scrollDistance = useRef(0)
+
   useEffect(() => {
     const updateScroll = (e: WheelEvent) => {
       const newDistance = e.pageY + scrollDistance.current
@@ -14,5 +15,8 @@ export default function useMouseWheel(
     }
     ref.current?.addEventListener('wheel', updateScroll)
   })
+
   return { eventObject: eventObjectRef.current }
 }
+
+//IDEA： 还可以代替Div的.feature.ts
