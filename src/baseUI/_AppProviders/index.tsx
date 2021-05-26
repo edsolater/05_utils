48985ca1b5ea1c306b7fe86d4c40079824e22c7c
zhanggongying <edsolater@qq.com>
2021-05-26 10:54:36 +0800

    提出一个想法： AppProviders

--- src/baseUI/_AppProviders/index.tsx
+++ src/baseUI/_AppProviders/index.tsx
@@ -2,18 +2,20 @@ import React, { FC } from 'react'
 import Div, { DivProps } from 'baseUI/Div'
 import { getCSS, CssProps } from './style'
 
-export interface ViewControllerProps extends DivProps, CssProps {
+// TODO: 没写页面， 只是理论上，这样可以
+// TODO： 只是占位，用于处理多Context的情况
+export interface AppProvidersProps extends DivProps, CssProps {
   /**
-   * 是否渲染子元素
+   * 是否渲染
    * @default true
    */
   if?: boolean
 }
 
-const _ViewController: FC<ViewControllerProps> = (props) => {
+const _AppProviders: FC<AppProvidersProps> = (props) => {
   const { if: ifExist = true } = props
 
   return ifExist ? <Div css={getCSS(props)}>{props.children}</Div> : null
 }
 
-export default _ViewController
+export default _AppProviders
