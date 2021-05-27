8e6bb6521ecb3aef675bdec40e2cc39e24d03d8d
zhanggongying <edsolater@qq.com>
2021-05-27 11:18:24 +0800

    useUpdateEffect.ts 中，要使用useEffect清理功能， 必须使用return

--- src/hooks/useUpdateEffect.ts
+++ src/hooks/useUpdateEffect.ts
@@ -3,14 +3,14 @@ import { useEffect, useRef } from 'react'
 /**
  * 与useEffect的区别是：初始化时，不触发回调
  */
-export default function useEffectInUpdate(
+export default function useUpdateEffect(
   effect: Parameters<typeof useEffect>[0],
   deps?: Parameters<typeof useEffect>[1]
 ) {
   const isInit = useRef(true)
   useEffect(() => {
     if (isInit.current === false) {
-      effect()
+      return effect()
     } else {
       isInit.current = false
     }
