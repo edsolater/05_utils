536d560a9f4d4cbe1178b3f6561968a05b4f7837
edsolater <edsolater@qq.com>
2021-05-29 20:30:29 +0800

    component: Image can now accept a list of srcs

--- src/baseUI/Image/index.tsx
+++ src/baseUI/Image/index.tsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import Div, { divProps, DivProps } from 'baseUI/Div'
+import React, { useState } from 'react'
+import { BaseUIDiv, divProps, DivProps } from 'baseUI/Div'
 import { mixCSSObjects } from 'style/cssParser'
 import { useImageStyle, ImageStyleProps } from './style'
 import pick from 'utils/object/pick'
+import isArray from 'utils/judgers/isArray'
 
 export interface ImageProps extends DivProps<'img'>, ImageStyleProps, ImageCoreProp {}
 export interface ImageCoreProp {
-  src?: string
+  /**
+   * 可接收 src 数组，代表不断的回退方案。（这些Src并不会同时下载）
+   */
+  src: string | string[]
   alt?: string
 }
-export const imageProps: (keyof ImageCoreProp)[] = ['src', 'alt']
 
 /**
  * @BaseUIComponent
  */
 const Image = (props: ImageProps) => {
   const { coreCss } = useImageStyle(props)
+  const [errorCount, setErrorCount] = useState(0)
+  const targetSrc = isArray(props.src)
+    ? props.src[errorCount] ?? props.src[props.src.length - 1]
+    : props.src
   return (
-    <Div
+    <BaseUIDiv
       as='img'
       {...pick(props, divProps)}
-      htmlProps={{ src: props.src, alt: props.alt, ...props.htmlProps }}
-      css={mixCSSObjects(props.css, coreCss)}
-    ></Div>
+      _htmlProps={{
+        src: targetSrc,
+        alt: props.alt,
+        onError: () => {
+          setErrorCount((n) => (n < props.src.length ? n + 1 : n))
+        }
+      }}
+      _css={mixCSSObjects(props.css, coreCss)}
+    />
   )
 }
 
