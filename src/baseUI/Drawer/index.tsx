4bb60b8cf84254489a2fd375007ccb3390537f01
edsolater <edsolater@qq.com>
2021-05-30 16:40:59 +0800

    feat：u can use <Draw.Mask> or it will render default<Draw.Mask>

--- src/baseUI/Drawer/index.tsx
+++ src/baseUI/Drawer/index.tsx
@@ -1,22 +1,11 @@
-import React, {
-  ComponentProps,
-  createContext,
-  FC,
-  PropsWithChildren,
-  ReactNode,
-  useContext,
-  useRef
-} from 'react'
-import Div, { DivProps } from 'baseUI/Div'
-import ReactDOM from 'react-dom'
-import cssColor from 'baseUI/__config/cssColor'
-import cssDefaults from 'baseUI/__config/cssDefaults'
-import useUpdateEffect from '../../hooks/useUpdateEffect'
-import createElementByString from './dom/createElementByString'
+import React, { ComponentProps, ReactElement, ReactNode } from 'react'
+import { DivProps } from 'baseUI/Div'
 import Mask from 'baseUI/Mask'
 import { ReactProps } from 'typings/constants'
 import DrawerCard from './DrawerCard'
-const DrawContext = createContext({})
+import createStore from 'baseUI/__hooks/createStore'
+import flat from 'utils/array/flat'
+const { Provider, useStore: useDrawerContext } = createStore({ currentMaskId: null })
 export interface DrawerCardProps extends DivProps {
   /**
    * 是否显示此drawer
@@ -50,31 +39,54 @@ export interface DrawerCardProps extends DivProps {
   // onCloseTransitionEnd?: (info: { el: HTMLDivElement }) => void
 }
 
+const _Drawer = (props: ReactProps<DrawerCardProps>) => {
+  // todo: 有个Drawer的占位，但还没开始写。估计会跟Mask纠缠在一起，需要多重考虑
+  const { isOpen, onClose } = props
+  const hasCustomedDrawerMask = reactChildrenContain(props.children, _DrawerMask)
+  return (
+    <>
+      {hasCustomedDrawerMask || (
+        <_DrawerMask maskBg='dodgerblue' /* TEMP */ isOpen={isOpen} onClose={onClose} />
+      )}
+      <_DrawerCard isOpen={isOpen}>{props.children}</_DrawerCard>
+    </>
+  )
+}
+
+const _DrawerMask = (props: ComponentProps<typeof Mask>) => {
+  return <Mask {...props} />
+}
+const _DrawerCard = (props: ComponentProps<typeof DrawerCard>) => {
+  return <DrawerCard {...props} />
+}
+
 /**
+ * @UIComponent 抽屉组件
+ *
  * 打开时，会生成一个 <Drawer> 在 drawer-root
  * (可能同时存在多个Drawer)
  * @todo 这里的实现虽然干净，但可能存在一堆没有 open 的 drawer
  */
-const Drawer = (props: ReactProps<DrawerCardProps>) => {
-  // todo: 有个Drawer的占位，但还没开始写。估计会跟Mask纠缠在一起，需要多重考虑
-  const { isOpen, needMask, onClose } = props
-  return (
-    <DrawContext.Provider value={{ isClosedByMask: 2 } /* TODO */}>
-      {/* FIXME: 应该有个<_MaskProtal>组件，不然，一个APP中只能存在一个Mask了 */}
-      {needMask && <Drawer.Mask isOpen={isOpen} onClose={onClose} />}
-      <Drawer.Card isOpen={isOpen}>{props.children}</Drawer.Card>
-    </DrawContext.Provider>
+const Drawer = (props) => (
+  <Provider>
+    <_Drawer {...props} />
+  </Provider>
+)
+/**
+ * @UIComponent
+ *
+ * 打开时，会生成一个 <Drawer> 在 drawer-root
+ * (可能同时存在多个Drawer)
+ * @todo 这里的实现虽然干净，但可能存在一堆没有 open 的 drawer
+ */
+Drawer.Mask = _DrawerMask
+Drawer.Card = _DrawerCard
+export default Drawer
+
+type ReactComponent = (...params) => ReactElement | null
+
+function reactChildrenContain(children: ReactNode, targetComponent: ReactComponent): boolean {
+  return flat([children]).some(
+    (item) => React.isValidElement(item) && item.type === targetComponent
   )
 }
-
-Drawer.Mask = (props: ComponentProps<typeof Mask>) => {
-  const hasDefaultMask = false
-  const drawerContectInfo = useContext(DrawContext)
-
-  return <Mask {...props} />
-}
-
-Drawer.Card = (props: ComponentProps<typeof DrawerCard>) => {
-  return <DrawerCard {...props} />
-}
-export default Drawer
--- src/pages/AllExamples/DrawerExample.tsx
+++ src/pages/AllExamples/DrawerExample.tsx
@@ -1,5 +1,5 @@
 import Button from 'baseUI/Button'
-import Drawer from 'baseUI/Drawer'
+import _Drawer from 'baseUI/Drawer'
 import React, { useState } from 'react'
 import ExampleCard from './ExampleCard'
 import ExampleGroup from './ExampleGroup'
@@ -8,19 +8,20 @@ import ExampleGroup from './ExampleGroup'
  * Button 的使用示例
  */
 const DrawerExample = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const [isDrawerOpen1, setIsDrawerOpen1] = useState(false)
+  const [isDrawerOpen2, setIsDrawerOpen2] = useState(false)
   return (
     <ExampleCard category='BaseUIComponent' title='Drawer'>
       <ExampleGroup caption='基础调用（自定义Mask）'>
-        <Drawer isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} needMask>
-          <Drawer.Mask isOpen={isDrawerOpen} />
-        </Drawer>
-        <Button onClick={() => setIsDrawerOpen((b) => !b)}>toggle</Button>
+        <_Drawer isOpen={isDrawerOpen1} onClose={() => setIsDrawerOpen1(false)} needMask>
+          <_Drawer.Mask isOpen={isDrawerOpen1} onClose={() => setIsDrawerOpen1(false)} />
+        </_Drawer>
+        <Button onClick={() => setIsDrawerOpen1((b) => !b)}>toggle</Button>
       </ExampleGroup>
 
       <ExampleGroup caption='基础调用（默认Mask）'>
-        <Drawer isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} needMask></Drawer>
-        <Button onClick={() => setIsDrawerOpen((b) => !b)}>toggle</Button>
+        <_Drawer isOpen={isDrawerOpen2} onClose={() => setIsDrawerOpen2(false)} needMask></_Drawer>
+        <Button onClick={() => setIsDrawerOpen2((b) => !b)}>toggle</Button>
       </ExampleGroup>
     </ExampleCard>
   )
