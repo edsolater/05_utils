90b271ccfbcdd2890a798ad4ddf9b211c8801301
edsolater <edsolater@qq.com>
2021-06-05 21:47:32 +0800

    chore: add Fall constructor in fall.ts

--- utils/functions/magic/fall.ts
+++ utils/functions/magic/fall.ts
@@ -1,4 +1,9 @@
 // SERENDIPITY: looks like this is Promise then chain
+// Ask so, why not just use promise.prototype.then()？
+
+import { AnyFn } from 'typings/constants'
+
+// because the value can't get syncly
 function fall<T, F1 extends (arg: T) => any>(n: T, actions: [F1]): ReturnType<F1>
 function fall<T, F1 extends (arg: T) => any, F2 extends (arg: ReturnType<F1>) => any>(
   n: T,
@@ -29,3 +34,24 @@ function fall(n, actions: any[]) {
   return actions.reduce((value, action) => action(value), n)
 }
 export default fall
+
+// TODO: test it !!!
+interface _Fall<O, T = O> {
+  originalValue: O
+  actionStack: AnyFn[]
+  then(cb: (nextValue: T) => any): _Fall<O, ReturnType<typeof cb>>
+  exec(): T
+}
+export function Fall<T>(value: T, stack?: AnyFn[]) {
+  const result: _Fall<T> = {
+    originalValue: value,
+    actionStack: stack ?? ([] as AnyFn[]),
+    then(cb) {
+      return Fall(value, result.actionStack.concat(cb))
+    },
+    exec() {
+      return result.actionStack.reduce((value, action) => action(value), result.originalValue)
+    }
+  }
+  return result
+}
