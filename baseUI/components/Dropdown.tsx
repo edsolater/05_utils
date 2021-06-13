be1830b2680fcfa762dfa342a4852358df64768f
edsolater <edsolater@qq.com>
2021-06-13 14:14:26 +0800

    chore(<Dropdown>): now have only props and sprops.

--- baseUI/components/Dropdown.tsx
+++ baseUI/components/Dropdown.tsx
@@ -4,48 +4,73 @@ import pick from 'utils/functions/object/pick'
 import Card, { CardProps } from './Card'
 import cache from 'utils/functions/functionFactory/cache'
 import { mixCSSObjects } from 'baseUI/style/cssParser'
+import { CSSPropertyValue } from 'baseUI/style/cssValue'
+import { CSSObject } from '@emotion/react'
+import cssDefaults from 'baseUI/settings/cssDefaults'
+import addDefault from 'utils/functions/magic/addDefault'
+import mergeObjects from 'utils/functions/object/mergeObjects'
+import { useAppSettings } from './AppSettings'
+import { mergeDeepObject } from 'utils/functions/merge'
 
 // 应该就是一种 Card 的特殊呈现形式
 /**
  * @BaseUIComponent
  */
-export interface DropdownProps extends DivProps, DropdownCSSProps {
+export interface DropdownProps extends DivProps {
   toggleBy?: 'click' | 'hover'
-  cardProps?: CardProps
+  props_Card?: CardProps
+  /**@cssProps */
+  distance?: CSSPropertyValue<'gap'>
 }
 
-interface DropdownCSSProps {}
+export interface DropdownSprops extends DropdownProps {
+  cardColor?: CSSPropertyValue<'color'>
+}
 
-const getCSS = cache((props: DropdownProps) =>
+const defaultSprops: DropdownSprops = {
+  distance: '16px',
+  cardColor: cssDefaults.whiteCard
+  // props_Card: { css: { background: cssDefaults.defaultBackgroundGray } } //TODO
+}
+
+const getCSS = cache((props: DropdownSprops) =>
   mixCSSObjects({
     position: 'relative'
   })
 )
-const getCardCSS = cache((props: DropdownProps) =>
+const getCardCSS = cache((props: DropdownSprops) =>
   mixCSSObjects({
     position: 'absolute',
-    top: 'calc(100% + 8px)'
+    background: props.cardColor,
+    top: `calc(100% + ${props.distance})`,
+    zIndex: '1'
   })
 )
 
 export default function Dropdown(props: DropdownProps) {
+  const appSettings = useAppSettings()
+  const sprops = addDefault(mergeObjects(props, appSettings.globalProps?.Dropdown), defaultSprops)
   const [opened, setopened] = useState(false)
-
+  console.log('sprops: ', sprops) // FIXME
   return (
     <BaseUIDiv
-      {...pick(props, divProps)}
+      {...pick(sprops, divProps)}
       _className='Dropdown'
-      _css={getCSS(props)}
-      _onHover={({ now }) => props.toggleBy === 'hover' && setopened(now === 'start')}
+      _css={getCSS(sprops)}
+      _onHover={({ now }) => sprops.toggleBy === 'hover' && setopened(now === 'start')}
       _onClick={() => setopened((b) => !b)}
     >
-      {props.children}
+      {sprops.children}
       {opened && ( //TODO：这个if逻辑导致每次都要重新加载整个卡片内容，就第一次是整体加载，然后都是显示/隐藏 //IDEA: 把这个显示的逻辑单独提取成一个组件
         // TODO: 要换成 HeadlessUI 的形式
         <Card
-          {...props.cardProps}
-          className='Dropdown-card'
-          css={[props.cardProps?.css, getCardCSS(props)]}
+          {...mergeDeepObject([
+            {
+              classNames: 'Dropdown-Card',
+              css: getCardCSS(sprops)
+            },
+            sprops.props_Card
+          ])}
         />
       )}
     </BaseUIDiv>
