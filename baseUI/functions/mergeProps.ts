369febbe5ec7ebe351899e293d4a936da818a1f2
edsolater <edsolater@qq.com>
2021-06-14 01:21:10 +0800

    type: make mergeProps more generic

--- baseUI/functions/mergeProps.ts
+++ baseUI/functions/mergeProps.ts
@@ -12,10 +12,27 @@ import mergeFunction from './mergeFunction'
 /**prop may very deep like children */
 export type AnyProp = { [props: string]: any }
 
-export default function mergeProps<P extends Array<MayDeepArray<AnyProp | undefined>>>(
-  ...propsObjs: P
-): any {
+export default function mergeProps<P1 = AnyProp, P2 = AnyProp>(
+  ...propsObjs: [P1, P2]
+): Exclude<P1 & P2, undefined>
+export default function mergeProps<P1 = AnyProp, P2 = AnyProp, P3 = AnyProp>(
+  ...propsObjs: [P1, P2, P3]
+): Exclude<P1 & P2 & P3, undefined>
+export default function mergeProps<P1 = AnyProp, P2 = AnyProp, P3 = AnyProp, P4 = AnyProp>(
+  ...propsObjs: [P1, P2, P3, P4]
+): Exclude<P1 & P2 & P3 & P4, undefined>
+export default function mergeProps<
+  P1 = AnyProp,
+  P2 = AnyProp,
+  P3 = AnyProp,
+  P4 = AnyProp,
+  P5 = AnyProp
+>(...propsObjs: [P1, P2, P3, P4, P5]): Exclude<P1 & P2 & P3 & P4 & P5, undefined>
+export default function mergeProps<P extends AnyProp | undefined>(
+  ...propsObjs: P[]
+): Exclude<P, undefined> {
   const trimedProps = flat(propsObjs).filter(notNullish)
+  // @ts-ignore
   if (trimedProps.length === 0) return {}
   if (trimedProps.length === 1) return trimedProps[0]
   return _mergeObjects(trimedProps, (key, v1, v2) =>
