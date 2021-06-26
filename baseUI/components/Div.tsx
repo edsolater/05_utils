de3d3c0fba4ce18b14be726783975366f90fd4f3
edsolater <edsolater@qq.com>
2021-06-27 01:06:46 +0800

    chore: get rid of isFragment , just use as(prop)

--- baseUI/components/Div.tsx
+++ baseUI/components/Div.tsx
@@ -1,4 +1,9 @@
-import  { createElement, CSSProperties, Fragment, ReactNode } from 'react'
+import React, {
+  createElement,
+  CSSProperties,
+  FC,
+  ReactNode
+} from 'react'
 import { useRef } from 'react'
 import { parseICSS } from '../style/cssParser'
 import { ICSS } from '../style/ICSS'
@@ -7,6 +12,8 @@ import useFeatureHover, { FeatureHoverOptions } from '../hooks/useFeatureHover'
 import mergeRefs, { IRefs } from '../functions/mergeRefs'
 import { MayDeepArray } from 'typings/tools'
 import useClick, { UseClickOptions } from '../hooks/useClick'
+import { isString, isUndefined } from 'utils/functions/judgers'
+import { omit } from 'utils/functions/object'
 
 /**
  *
@@ -26,9 +33,8 @@ export interface TagMap {
 // 设立BaseProps是为了给其他baseUI如Img用的
 export interface DivProps<TagName extends keyof TagMap = 'div'> {
   // 只能低层组件使用
-  as?: TagName
+  as?: TagName | FC | typeof React.Fragment
   domRef?: IRefs<TagMap[TagName]>
-  isFragment?: boolean // 该节点的行为，就像 React.Fragnment
   css?: ICSS
   className?: MayDeepArray<ClassName>
   style?: CSSProperties
@@ -38,19 +44,22 @@ export interface DivProps<TagName extends keyof TagMap = 'div'> {
   children?: ReactNode
 }
 
+// TODO: as为组件时 的智能推断还不够好
 export const Div = <TagName extends keyof TagMap = 'div'>(props: DivProps<TagName>) => {
   const divRef = useRef<TagMap[TagName]>(null)
   useClick<TagMap[TagName]>(divRef, { onClick: props.onClick })
   useFeatureHover(divRef, { onHover: props.onHover })
-  if (props.isFragment) return createElement(Fragment, { children: props.children })
-  const allProps = {
-    ...props.htmlProps,
-    children: props.children,
-    className: classname(props.className) + parseICSS(props.css),
-    ref: mergeRefs(props.domRef, divRef),
-    style: props.style
-  }
-  return createElement(props.as ?? 'div', allProps)
+
+  return isUndefined(props.as) || isString(props.as)
+    ? createElement(props.as ?? 'div', {
+        ...props.htmlProps,
+        children: props.children,
+        className: classname(props.className) + parseICSS(props.css),
+        ref: mergeRefs(props.domRef, divRef),
+        style: props.style
+      })
+      // @ts-expect-error don't know why
+    : createElement(props.as, omit(props, 'as'))
 }
 
 export default Div
