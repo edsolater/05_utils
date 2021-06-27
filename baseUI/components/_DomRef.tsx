8975ab639ad6ff5584f70760b0896cf62f2f98b4
edsolater <edsolater@qq.com>
2021-06-27 13:03:24 +0800

    feat: <_DomRef> use <_Props>

--- baseUI/components/_DomRef.tsx
+++ baseUI/components/_DomRef.tsx
@@ -1,19 +1,14 @@
 import { mergeProps } from 'baseUI/functions'
-import React, { isValidElement, RefObject } from 'react'
+import React, { RefObject } from 'react'
 import { MayArray } from 'typings/tools'
-import { isArray } from 'utils/functions/judgers'
 import { DivProps } from './Div'
+import _Props from './_Props'
 
 interface _DomRefProps extends DivProps {
   _domRef?: MayArray<RefObject<HTMLElement | null | undefined>>
 }
 
 export default function _DomRef({ _domRef, children, ...restProps }: _DomRefProps) {
-  if (isArray(children)) {
-    throw new Error(`<${_DomRef.name}> can't accept an array of child`)
-  }
-  if (!isValidElement(children)) {
-    throw new Error(`the direct child of <${_DomRef.name}> is not ReactElement`)
-  }
-  return React.cloneElement(children, mergeProps(restProps, children.props, { domRef: _domRef }))
+  _Props.checkChildren(children, _DomRef.name)
+  return <_Props {...mergeProps(restProps, { domRef: _domRef })}>{children}</_Props>
 }
--- baseUI/components/_Props.tsx
+++ baseUI/components/_Props.tsx
@@ -1,4 +1,4 @@
-import React, { isValidElement } from 'react'
+import React, { isValidElement, ReactElement, ReactNode } from 'react'
 import { isArray } from 'utils/functions/judgers'
 import { DivProps } from './Div'
 import { mergeProps } from 'baseUI/functions'
@@ -8,12 +8,18 @@ interface _PropsProps extends DivProps {
 }
 
 export default function _Props({ children, ...restProps }: _PropsProps) {
-  children
+  _Props.checkChildren(children, _Props.name)
+  return React.cloneElement(children, mergeProps(restProps, children.props))
+}
+
+_Props.checkChildren = function (
+  children: ReactNode,
+  componentName: string
+): asserts children is ReactElement {
   if (isArray(children)) {
-    throw new Error(`<${_Props.name}> can't accept an array of child`)
+    throw new Error(`<${componentName}> can't accept an array of child`)
   }
   if (!isValidElement(children)) {
-    throw new Error(`the direct child of <${_Props.name}> is not ReactElement`)
+    throw new Error(`the direct child of <${componentName}> is not ReactElement`)
   }
-  return React.cloneElement(children, mergeProps(restProps, children.props))
 }
