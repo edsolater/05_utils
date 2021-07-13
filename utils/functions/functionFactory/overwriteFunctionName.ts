1d2039c9b3a09dd8a966eea28157f749c4e258bb
zhanggongying <edsolater@qq.com>
2021-07-13 16:10:22 +0800

    style: change overwriteFunctionName

--- utils/functions/functionFactory/overwriteFunctionName.ts
+++ utils/functions/functionFactory/overwriteFunctionName.ts
@@ -1,11 +1,11 @@
 /**
- * 设定新函数名，并返回新函数
- * @param func 目标函数
- * @param name 设定的函数名
+ * @example
+ * const origionFn = () => {}
+ * overwriteFunctionName(originFn, 'newName') //=> newNameFunction
  */
-export default function overwriteFunctionName<F extends Function>(func: F, name: string): F {
+ export default function overwriteFunctionName<F extends (...params: any[]) => any>(func: F, name: string): F {
   const temp = {
-    [name]: (...args) => func(...args)
+    [name]: (...args: any[]) => func(...args)
   }
-  return temp[name] as any
+  return temp[name] as F
 }
