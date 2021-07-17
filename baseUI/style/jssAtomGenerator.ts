4eae3f7694f268365cffad2069af4f0b05deb706
edsolater <edsolater@qq.com>
2021-07-18 00:34:55 +0800

    chore(jssAtom): simplier the design

--- baseUI/style/jssAtomGenerator.ts
+++ baseUI/style/jssAtomGenerator.ts
@@ -1,20 +1,19 @@
 import { CSSProperties } from 'react'
-import { ArrayItem, ExtractProperty, PascalCase, SKeyof, SnakeCase } from 'typings/tools'
+import { ArrayItem, ExtractProperty, SKeyof, SnakeCase } from 'typings/tools'
 import { objectFlatMapEntry } from '../../utils/functions/object/objectMap'
 import { overwriteFunctionName } from '../../utils/functions/functionFactory'
-import { toCamelCase, toPascalCase } from '../../utils/functions/string/changeCase'
+import { toCamelCase, toPascalCase, toSnakeCase } from '../../utils/functions/string/changeCase'
 import { toPxIfNumber } from './cssUnits'
-import { isString } from 'utils/functions/judgers'
+import { isString } from '../../utils/functions/judgers'
 
 const globalKeywords = ['inherit', 'initial', 'revert', 'unset'] as const
 
-type Keyword = string | readonly [keyword: string, params: readonly string[]]
+type Keyword = readonly [jskeyword: string, cssValue: string]
 type GetKeywordString<K extends Keyword> = K extends string ? K : K[0]
 
 type GetJSSAtomFromOptions<
   PropertiesOptions extends {
     [P in keyof CSSProperties]: {
-      keywordPrefix?: string
       keywords: readonly Keyword[]
     }
   }
@@ -29,12 +28,7 @@ type GetJSSAtomFromOptions<
   } &
     /* shortcut css rule */
     {
-      [JSSPropertyName in SKeyof<PropertiesOptions> as `${ExtractProperty<
-        PropertiesOptions[JSSPropertyName],
-        'keywordPrefix',
-        JSSPropertyName
-      > &
-        string}_${SnakeCase<
+      [JSSPropertyName in SKeyof<PropertiesOptions> as `${SnakeCase<
         GetKeywordString<ArrayItem<ExtractProperty<PropertiesOptions[JSSPropertyName], 'keywords'>>>
       >}`]: {
         [K in JSSPropertyName]: string
@@ -49,9 +43,9 @@ export default function jssAtomGenerator<
     [P in keyof CSSProperties]: { keywordPrefix?: string; keywords: readonly Keyword[] }
   }
 >({ properties }: { properties: PropertiesOptions }): GetJSSAtomFromOptions<PropertiesOptions> {
-  return objectFlatMapEntry(properties, ([propertyName, { keywords }]) => [
+  return objectFlatMapEntry(properties, ([propertyName, { keywords = [] } = {}]) => [
     [propertyName, getMainFunction(propertyName)],
-    getShortcutFunctions(propertyName, [...keywords, ...globalKeywords]).map((fn) => [fn.name, fn])
+    ...getShortcutFunctions(propertyName, keywords)
   ])
 }
 
@@ -62,14 +56,9 @@ function getMainFunction(cssPropertyName: string) {
   )
 }
 
-function getShortcutFunctions(cssPropertyName: string, keywords: Keyword[]) {
-  const verboseKeywordInput = keywords.map((keyword) =>
-    isString(keyword) ? [keyword, [keyword]] : keyword
-  )
-  return verboseKeywordInput.map(([keywordName, params]) =>
-    overwriteFunctionName(
-      () => ({ [toCamelCase(cssPropertyName)]: (params as string[]).map(toPxIfNumber).join(' ') }),
-      `${toCamelCase(cssPropertyName)}${toPascalCase(keywordName as string)}`
-    )
-  )
+function getShortcutFunctions(cssPropertyName: string, keywords: readonly Keyword[]) {
+  return keywords.map(([keywordName, cssValue]) => [
+    toSnakeCase(keywordName),
+    { [toCamelCase(cssPropertyName)]: cssValue }
+  ])
 }
--- baseUI/style/jssAtoms.ts
+++ baseUI/style/jssAtoms.ts
@@ -2,29 +2,79 @@ import jssAtomGenerator from './jssAtomGenerator'
 
 /**
  * @namespace JSSAtoms
+ * mostly it's tailwind style
  */
 const JSSAtoms = jssAtomGenerator({
   properties: {
-    backgroundSize: { keywords: ['cover', 'contain'] },
-    backgroundOrigin: { keywords: ['border-box', 'padding-box', 'content-box'] },
-    backgroundRepeat: {
-      keywordPrefix: 'bg',
-      keywords: ['repeat-x', 'repeat-y', 'repeat', 'space', 'round', 'no-repeat']
-    },
-    backgroundPosition: {
-      keywordPrefix: 'bg', // so you can just call bg_top() bg_top_left()
+    /**
+     * @see tilwindCSS https://tailwindcss.com/docs/background-size
+     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-size#syntax
+     */
+    backgroundSize: {
       keywords: [
-        'top',
-        'bottom',
-        'left',
-        'right',
-        'center',
-        ['top_left', ['top', 'left']],
-        ['top_right', ['top', 'right']]
+        ['bg_auto', 'auto'],
+        ['bg_cover', 'cover'],
+        ['bg_contain', 'contain']
+      ]
+    },
+
+    /**
+     * @see tilwindCSS https://tailwindcss.com/docs/background-origin
+     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-origin#syntax
+     */
+    backgroundOrigin: {
+      keywords: [
+        ['bg_origin_border', 'border-box'],
+        ['bg_origin_padding', 'padding-box'],
+        ['bg_origin_content', 'content-box']
+      ]
+    },
+
+    /**
+     * @see tilwindCSS https://tailwindcss.com/docs/background-repeat
+     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-repeat#syntax
+     */
+    backgroundRepeat: {
+      keywords: [
+        ['bg_repeat', 'repeat'],
+        ['bg_no_repeat', 'no-repeat'],
+        ['bg_repeat_x', 'repeat-x'],
+        ['bg_repeat_y', 'repeat-y'],
+        ['bg_repeat_round', 'round'],
+        ['bg_repeat_space', 'space']
+      ]
+    },
+
+    /**
+     * @see tilwindCSS https://tailwindcss.com/docs/background-clip
+     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-clip#syntax
+     */
+    backgroundClip: {
+      keywords: [
+        ['bg_clip_border', 'border-box'],
+        ['bg_clip_padding', 'padding-box'],
+        ['bg_clip_content', 'content-box'],
+        ['bg_clip_text', 'text']
+      ]
+    },
+
+    /**
+     * @see tilwindCSS https://tailwindcss.com/docs/background-position
+     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-position#syntax
+     */
+    backgroundPosition: {
+      keywords: [
+        ['bg_top', 'top'],
+        ['bg_bottom', 'bottom'],
+        ['bg_left', 'left'],
+        ['bg_right', 'right'],
+        ['bg_center', 'center'],
+        ['bg_top_left', 'top left'],
+        ['bg_top_right', 'top right']
       ]
     }
   }
 } as const)
-export default JSSAtoms.backgroundPosition
+export default JSSAtoms
 
-console.log(JSSAtoms)
+// console.log('JSSAtoms: ', JSSAtoms.bg_origin_border)
