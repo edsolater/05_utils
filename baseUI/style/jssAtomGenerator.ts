cf643dc29b019bd9696e8282a65eb4882089727d
edsolater <edsolater@qq.com>
2021-07-18 02:39:46 +0800

    chore(jssAtom): add variants

--- baseUI/style/jssAtomGenerator.ts
+++ baseUI/style/jssAtomGenerator.ts
@@ -5,23 +5,26 @@ import { overwriteFunctionName } from '../../utils/functions/functionFactory'
 import { toSnakeCase } from '../../utils/functions/string/changeCase'
 import { toPxIfNumber } from './cssUnits'
 
+type AtomCSSRule = {
+  [csskey: string]: any
+}
+
+type AtomRule = [jssKeywordName: string, cssRule: AtomCSSRule]
+
 type Keyword = readonly [jskeyword: string, cssValue: string]
+type Variant = string
+
 type PropertyOption = {
   [P in keyof CSSProperties]: {
-    keywordPrefix?: string
     keywords: readonly Keyword[]
   }
 }
-type GetJSSAtomFromOptions<
-  PropertiesOptions extends {
-    [P in keyof CSSProperties]: {
-      keywords: readonly Keyword[]
-    }
-  }
-> =
+type VariantOption = readonly Variant[]
+
+type GetJSSAtomFromOptions<P extends PropertyOption, V extends VariantOption> =
   /* main css func */
   {
-    [JSSPropertyName in SKeyof<PropertiesOptions>]: (
+    [JSSPropertyName in SKeyof<P>]: (
       ...cssInputs: (string | number)[]
     ) => {
       [K in JSSPropertyName]: string
@@ -29,37 +32,55 @@ type GetJSSAtomFromOptions<
   } &
     /* shortcut css rule */
     {
-      [JSSPropertyName in SKeyof<PropertiesOptions> as `${SnakeCase<
-        ExtractProperty<PropertiesOptions[JSSPropertyName], 'keywords'>[number][0]
-      >}`]: {
+      [JSSPropertyName in SKeyof<P> as `${ExtractProperty<
+        P[JSSPropertyName],
+        'keywords'
+      >[number][0]}`]: {
         [K in JSSPropertyName]: string
+      }
+    } &
+    {
+      /* variant added css rule */
+      [JSSPropertyName in SKeyof<P> as `${V[number]}:${ExtractProperty<
+        P[JSSPropertyName],
+        'keywords'
+      >[number][0]}`]: {
+        [K in `:hover`]: { [P in JSSPropertyName]: string }
       }
     }
 
 /**
  * how to use the function ? Please see {@link JSSAtoms}
  */
-export default function jssAtomGenerator<P extends PropertyOption>({
-  properties
+export default function jssAtomGenerator<P extends PropertyOption, V extends VariantOption>({
+  properties,
+  variants
 }: {
   properties: P
-}): GetJSSAtomFromOptions<P> {
-  return objectFlatMapEntry(properties, ([propertyName, { keywords = [] } = {}]) => [
-    [propertyName, getMainFunction(propertyName)],
-    ...getShortcutFunctions(propertyName, keywords)
-  ])
+  variants: V
+}): GetJSSAtomFromOptions<P, V> {
+  return objectFlatMapEntry(properties, ([propertyName, { keywords = [] } = {}]) =>
+    addVariant(variants, [
+      [propertyName, getMainFunction(propertyName)],
+      ...getShortcutFunctions(propertyName, keywords)
+    ])
+  )
 }
 
-function getMainFunction(cssPropertyName: string) {
+function getMainFunction(cssPropertyName: string): (...params: any[]) => AtomCSSRule {
   return overwriteFunctionName(
-    (...params) => ({ [cssPropertyName]: params.map(toPxIfNumber).join(' ') }),
+    (...params) => ({ [cssPropertyName]: params.map(toPxIfNumber).join(' ') } as AtomCSSRule),
     cssPropertyName
   )
 }
 
-function getShortcutFunctions(cssPropertyName: string, keywords: readonly Keyword[]) {
-  return keywords.map(([keywordName, cssValue]) => [
-    toSnakeCase(keywordName),
-    { [cssPropertyName]: cssValue }
-  ])
+function getShortcutFunctions(cssPropertyName: string, keywords: readonly Keyword[]): AtomRule[] {
+  return keywords.map(([keywordName, cssValue]) => [keywordName, { [cssPropertyName]: cssValue }])
+}
+
+function addVariant(variants: readonly Variant[], originAtomRules: readonly AtomRule[]) {
+  const variantedAtomRules = originAtomRules.flatMap(([key, cssRule]) =>
+    variants.map((variant) => [`${variant}:${key}`, { [`:${variant}`]: cssRule }] as AtomRule)
+  )
+  return [...originAtomRules, ...variantedAtomRules]
 }
--- baseUI/style/jssAtoms.ts
+++ baseUI/style/jssAtoms.ts
@@ -6,57 +6,57 @@ import jssAtomGenerator from './jssAtomGenerator'
  */
 const JSSAtoms = jssAtomGenerator({
   properties: {
-    /**
-     * @see tilwindCSS https://tailwindcss.com/docs/background-size
-     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-size#syntax
-     */
-    backgroundSize: {
-      keywords: [
-        ['bg-auto', 'auto'],
-        ['bg-cover', 'cover'],
-        ['bg-contain', 'contain']
-      ]
-    },
+    // /**
+    //  * @see tilwindCSS https://tailwindcss.com/docs/background-size
+    //  * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-size#syntax
+    //  */
+    // backgroundSize: {
+    //   keywords: [
+    //     ['bg-auto', 'auto'],
+    //     ['bg-cover', 'cover'],
+    //     ['bg-contain', 'contain']
+    //   ]
+    // },
 
-    /**
-     * @see tilwindCSS https://tailwindcss.com/docs/background-origin
-     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-origin#syntax
-     */
-    backgroundOrigin: {
-      keywords: [
-        ['bg-origin-border', 'border-box'],
-        ['bg-origin-padding', 'padding-box'],
-        ['bg-origin-content', 'content-box']
-      ]
-    },
+    // /**
+    //  * @see tilwindCSS https://tailwindcss.com/docs/background-origin
+    //  * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-origin#syntax
+    //  */
+    // backgroundOrigin: {
+    //   keywords: [
+    //     ['bg-origin-border', 'border-box'],
+    //     ['bg-origin-padding', 'padding-box'],
+    //     ['bg-origin-content', 'content-box']
+    //   ]
+    // },
 
-    /**
-     * @see tilwindCSS https://tailwindcss.com/docs/background-repeat
-     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-repeat#syntax
-     */
-    backgroundRepeat: {
-      keywords: [
-        ['bg-repeat', 'repeat'],
-        ['bg-no-repeat', 'no-repeat'],
-        ['bg-repeat-x', 'repeat-x'],
-        ['bg-repeat-y', 'repeat-y'],
-        ['bg-repeat-round', 'round'],
-        ['bg-repeat-space', 'space']
-      ]
-    },
+    // /**
+    //  * @see tilwindCSS https://tailwindcss.com/docs/background-repeat
+    //  * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-repeat#syntax
+    //  */
+    // backgroundRepeat: {
+    //   keywords: [
+    //     ['bg-repeat', 'repeat'],
+    //     ['bg-no-repeat', 'no-repeat'],
+    //     ['bg-repeat-x', 'repeat-x'],
+    //     ['bg-repeat-y', 'repeat-y'],
+    //     ['bg-repeat-round', 'round'],
+    //     ['bg-repeat-space', 'space']
+    //   ]
+    // },
 
-    /**
-     * @see tilwindCSS https://tailwindcss.com/docs/background-clip
-     * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-clip#syntax
-     */
-    backgroundClip: {
-      keywords: [
-        ['bg-clip-border', 'border-box'],
-        ['bg-clip-padding', 'padding-box'],
-        ['bg-clip-content', 'content-box'],
-        ['bg-clip-text', 'text']
-      ]
-    },
+    // /**
+    //  * @see tilwindCSS https://tailwindcss.com/docs/background-clip
+    //  * @see MDN https://developer.mozilla.org/en-US/docs/Web/CSS/background-clip#syntax
+    //  */
+    // backgroundClip: {
+    //   keywords: [
+    //     ['bg-clip-border', 'border-box'],
+    //     ['bg-clip-padding', 'padding-box'],
+    //     ['bg-clip-content', 'content-box'],
+    //     ['bg-clip-text', 'text']
+    //   ]
+    // },
 
     /**
      * @see tilwindCSS https://tailwindcss.com/docs/background-position
@@ -73,8 +73,9 @@ const JSSAtoms = jssAtomGenerator({
         ['bg-top-right', 'top right']
       ]
     }
-  }
+  },
+  variants: ['hover', 'active']
 } as const)
 export default JSSAtoms
 
-console.log('JSSAtoms: ', JSSAtoms.bg_origin_border)
+console.log('JSSAtoms: ', JSSAtoms['hover:bg-bottom'])
