a0981665389b8676ac7b01e57f6d232393ddea08
zhanggongying <edsolater@qq.com>
2021-07-20 11:47:24 +0800

    chore(TableList): it's hard to type with class.  (maybe with V8, i shouldn't use Class any more)

--- utils/containers/TableList.ts
+++ utils/containers/TableList.ts
@@ -1,34 +1,80 @@
-// abstract class ArrayBase<T> implements Array<T> {
-//   #innerArr: T[]
-//   constructor(abstractOption: {
-//     init?: T[]
-//     proxyHandler?: { set?(prop, value): any; get?(prop): any }
-//   }) {
-//     this.#innerArr = new Proxy(abstractOption.init ?? [], abstractOption.proxyHandler??{})
-//   }
-//   get length() {
-//     return this.#innerArr.length
-//   }
-//   push(item) {
-//     return this.#innerArr.push(item)
-//   }
-// }
-class TableList<T> implements Array<T> {
-  raw: T[]
-  // #indexList1: {
-  //   keyName: keyof T
-  //   list: Map<T[keyof T], T>
-  // }
+interface BasicArrayLike<T> {
+  length: number
+  push(item: T): void
+  toArray(): T[]
+
+  map<U = any>(mapper: (item: T, idx: number) => U): BasicArrayLike<U>
+  reduce(
+    callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T,
+    initialValue?: T
+  ): T
+  reduce<U>(
+    callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U,
+    initialValue?: U
+  ): U
+
+  [Symbol.iterator](): IterableIterator<T>
+  entries(): IterableIterator<[number, T]>
+  keys(): IterableIterator<number>
+  values(): IterableIterator<T>
+}
+
+interface TableList<T> extends BasicArrayLike<T> {
+  $raw: T[]
+  constructor(init: T[], options?: { key: (keyof T)[] })
+  length: number
+}
+
+class TableList<T> {
+  $raw: T[]
   constructor(init: T[], option?: { key: (keyof T)[] }) {
-    
+    this.$raw = new Proxy(Array.from(init), {
+      /* TODO */
+    })
+  }
+  get length() {
+    return this.$raw.length
+  }
+  push(item: T) {
+    this.$raw.push(item)
+  }
+  toArray() {
+    return this.$raw
+  }
+
+  //@ts-ignore don't want to type-info in javascript filed
+  map(...params) {
+    //@ts-ignore don't want to type-info in javascript filed
+    return new TableList(this.$raw.map(...params))
+  }
+  reduce(callbackfn, initialValue) {
+    return this.$raw.reduce(callbackfn, initialValue)
+  }
+
+  *[Symbol.iterator]() {
+    yield* this.$raw
+  }
+  entries() {
+    return this.$raw.entries()
+  }
+  keys() {
+    return this.$raw.keys()
+  }
+  values() {
+    return this.$raw.values()
   }
 
   // establishIndex()
 }
+
 const foo = new TableList([
   { key: 2, name: 'sdf' },
   { key: 5, name: 'hello' }
 ])
 foo.push({ key: 2, name: 'sdf' })
+foo.map(({ key }) => key)
 const a = [...foo]
 console.log('foo.getArr: ', a)
+
+const b = [1, 2, 3]
+const bb = b.map((i) => String(i))
