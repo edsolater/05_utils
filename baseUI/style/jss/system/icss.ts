199db5d7e08f1cb989bd854b44344feb66a57ed3
edsolater <edsolater@qq.com>
2021-07-24 20:04:38 +0800

    chore: move temp to jss-atom

--- baseUI/style/jss/system/icss.ts
+++
@@ -1,7 +0,0 @@
-import { CSSObject } from '@emotion/react'
-import { CSSProperties } from 'react'
-import { MayArray } from 'typings/tools'
-
-
-export type ICSS = MayArray<ICSSObject | boolean | string | number | null | undefined>
-
--- baseUI/temp/toKebab.ts
+++
@@ -1,5 +0,0 @@
-function toKebab(prop: string) {
-  return prop.replace(/[A-Z]/g, '-$&').toLowerCase()
-}
-
-console.log(toKebab('helloWorldisdfI'))
